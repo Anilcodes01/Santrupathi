970526adfa2920d7843de128bf7dafc7cfb1c526
Anil Pal <anilcodes01@gmail.com>
2025-02-16 17:58:58 +0530

    completed the second section

--- components/HeroComponents/second_section/Section.tsx
+++ components/HeroComponents/second_section/Section.tsx
@@ -2,20 +2,36 @@ import Image from "next/image";
 import PureBite from "./pure_bite";
 import WhyBread from "./why_bread";
 
-
 export default function SecSection() {
+  return (
+    <div className="hide-scrollbar">
+      <div>
+        <PureBite />
+      </div>
 
-    return <div className="hide-scrollbar">
-       <div>
-       <PureBite />
-       </div>
+      <div className=" flex items-end justify-end lg:-mt-20">
+        <Image
+          src="/pngwing.com (2).png"
+          width={500}
+          height={500}
+          alt=" wheat.png"
+          className="w-64 h-64"
+        />
+      </div>
 
-        <div className=" flex items-end justify-end">
-            <Image src="/pngwing.com (2).png" width={500} height={500} alt=" wheat.png" className="w-64 h-64"/>
-        </div>
+      <div className="-mt-32">
+        <WhyBread />
+      </div>
 
-        <div>
-            <WhyBread />
-        </div>
+      <div className="flex items-end justify-start -mt-32">
+        <Image
+          src="/carrot1.png"
+          width={500}
+          height={500}
+          alt="carrot"
+          className="w-96 h-96"
+        />
+      </div>
     </div>
+  );
 }
